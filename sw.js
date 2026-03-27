dd23c75ff810921c059305b7a8ccd259136119f6
Your Name <you@example.com>
2026-03-27 17:35:00 -0300

    feat: implement PWA features for mobile app experience

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'cobra-io-v1';
+const ASSETS = [
+  '/',
+  '/cobra.html',
+  '/icon-512.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
